853adfcceb430a18a53edf7bc57f25461e0f3695
naiemhosen430 <naiemhosen430@gmail.com>
2024-09-08 23:13:04 +0600

    d

--- src/modules/auth/auth.controler.js
+++ src/modules/auth/auth.controler.js
@@ -74,7 +74,7 @@ export const createUserController = async (req, res) => {
     console.log(error);
     return res.status(500).json({
       statusCode: 500,
-      message: error,
+      error,
     });
   }
 };
